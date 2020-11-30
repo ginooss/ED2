c841f46d477a2fa967ee79bbed6a94630f376c2b
Joao <joaovitorsrg@hotmail.com>
2020-11-30 16:02:54 -0300

    atividade 10-11

---
+++ Atividade.10.11/Atividade.10.11/Venda.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade._10._11
+{
+    class Venda
+    {
+        private int qtde;
+        private double valor;
+
+        public int Qtde
+        {
+            get { return qtde; }
+            set { qtde = value; }
+        }
+
+        public double Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+        public Venda(int qtde, double valor)
+        {
+            this.qtde = qtde;
+            this.valor = valor;
+        }
+
+        public Venda() : this(0, 0.0)
+        {
+        }
+
+        public double valorMedio()
+        {
+            return valor / qtde;
+        }
+    }
+}
