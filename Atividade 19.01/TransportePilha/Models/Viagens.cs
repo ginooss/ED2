c189bd6a0b55e4a9c4485741d7faa163ca648526
ginooss <joaovitorsrg@hotmail.com>
2021-01-27 12:05:49 -0300

    a

---
+++ Atividade 19.01/TransportePilha/Models/Viagens.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportePilha.Models
+{
+    class Viagens
+    {
+        private Queue<Viagem> viagens;
+
+        public void incluir(Viagem viagem)
+        {
+            viagem.Id = viagens.Count + 1;
+            viagens.Enqueue(viagem);
+        }
+    }
+}
